70ff49df05e34fc64f5e91bdd53d2151d26f0906
krakra9 <krakra9@protonmail.com>
2024-03-15 17:21:22 +0000

    fixing typo

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var app = express()
 app.get("/",function(request,response){response.send("Hello World!")})
 app.listen(3000, function () {
     console.log("Started application on port %d", 3000)
-});s
+});
