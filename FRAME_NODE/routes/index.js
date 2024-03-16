ce111f37fd33c4d6059edf3bdf878e1b44c4e376
krakra9 <krakra9@protonmail.com>
2024-03-16 13:01:56 +0000

    removing endpoint bug

--- FRAME_NODE/routes/index.js
+++ FRAME_NODE/routes/index.js
@@ -1,10 +1,5 @@
 var express = require('express');
 var router = express.Router();
-import {getSSLHubRpcClient, Message} from "@farcaster/hub-nodejs";
-
-
-const client = HUB_URL ? getSSLHubRpcClient(HUB_URL) : undefined;
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,10 +14,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/', async function(req, res, next) {
   console.log(req)
-  const frameMessage = Message.decode(Buffer.from(req.body?.trustedData?.messageBytes || '', 'hex'));
-  const result = await client?.validateMessage(frameMessage);
-
-  res.render('index', { title: 'Express' });
+  res.render('results', { title: 'Express' });
 });
 
 
