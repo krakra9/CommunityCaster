d6d02575fe8cc30a3d5a644e5a1163c1194dcdb4
krakra9 <krakra9@protonmail.com>
2024-03-16 13:00:48 +0000

    Adding post endpoint to frame

--- FRAME_NODE/routes/index.js
+++ FRAME_NODE/routes/index.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var router = express.Router();
+import {getSSLHubRpcClient, Message} from "@farcaster/hub-nodejs";
+
+
+const client = HUB_URL ? getSSLHubRpcClient(HUB_URL) : undefined;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if (req.body?.trustedData) {
+    // has not voted
+    res.render('index', { title: 'Express' });
+  }
+  else {
+    res.render('results', { title: 'Express' });
+  }
+});
+
+router.post('/', async function(req, res, next) {
+  console.log(req)
+  const frameMessage = Message.decode(Buffer.from(req.body?.trustedData?.messageBytes || '', 'hex'));
+  const result = await client?.validateMessage(frameMessage);
+
   res.render('index', { title: 'Express' });
 });
+
+
+
 
 
 module.exports = router;
